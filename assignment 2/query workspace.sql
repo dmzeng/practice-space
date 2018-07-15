5d44e47738890a53cbeb0ed998a55080798fcec6
dmzeng <40397678+dmzeng@users.noreply.github.com>
2018-07-15 13:02:47 -0700

    Update query workspace.sql

--- assignment 2/query workspace.sql
+++ assignment 2/query workspace.sql
@@ -1,3 +1,29 @@
+#Select an org's bin_weights for a specified time period
+
+SELECT bin_weights.*
+FROM bin_weights, clusters, orgs
+WHERE bin_weights.unit_id = clusters.id AND clusters.org = orgs.id AND orgs.name = '[org name]' AND 
+    bin_weights.ignore = 'f' AND bin_weights.change > 0.200001;
+
+#Select a site's bin_weights for a specified time period
+
+SELECT bin_weights.*
+FROM bin_weights, clusters, sites
+WHERE bin_weights.unit_id = clusters.id AND clusters.site = sites.id AND sites.name = 'Pinterest NY'
+    AND bin_weights.ignore = 'f' AND bin_weights.change > 0.200001;
+
+#Copy an org's bin_weights for a specified time period to a CSV file
+#Newlines causing conflicts with the copy command, so they're omitted from these query.
+#Note: target csv file should be named with the pattern org_name-year-month.csv, i.e. 'pinterest_2018-may.csv'
+
+\copy ( SELECT bin_weights.* FROM bin_weights, clusters, sites WHERE bin_weights.unit_id = clusters.id AND clusters.org = orgs.id AND orgs.name = '[org name]' AND bin_weights.timestamp BETWEEN '[start date]' AND '[end date]' AND bin_weights.ignore = 'f' AND bin_weights.change > 0.200001 ) TO '/home/evoeco/[file name].csv' DELIMITER ',' CSV;
+
+#Copy a site's bin_weights for a specified time period to a CSV file
+
+\copy ( SELECT bin_weights.* FROM bin_weights, clusters, sites WHERE bin_weights.unit_id = clusters.id AND clusters.site = sites.id AND sites.name = '[site name]' AND bin_weights.timestamp BETWEEN '[start date]' AND '[end date]' AND bin_weights.ignore = 'f' AND bin_weights.change > 0.200001 ) TO '/home/evoeco/[file name].csv' DELIMITER ',' CSV;
+
+
+
 # I'm avoiding * because it seems like it could potentially be an issue
 # I think it is inefficient to join three tables and get all relevant tuples, so ideally
 # we should prune sites by a "org name" parameter, then join to clusters, prune by a
@@ -9,28 +35,26 @@
 # 	mode, change in weight, total weight, overweight status, ignore status) sorted chronologically
 # note: since awd_ids is an integer array, I'm not sure if I can export that in a .csv 
 
-# Mode Information
-# Unknown = 0
-# Landfill = 1,
-# Compost = 2,
-# Recycle = 3,
-# Mixed Paper = 4
-
-SELECT b.unit_id as Cluster ID 
-	, c.name as Cluster Name
-	, site.name as Organization 
-	, b.timestamp as Timestamp 
-	, b.mode as Bin mode
-	, b.change as Weight change
-	, b.weight as Total Weight
-	, b.overweight as Overweight Status
-	, b.ignore as Ignore Status 
+SELECT b.unit_id as Cluster_ID
+	, c.name as Cluster_Name
+	, s.name as Organization 
+	, b.timestamp as Time_of_Event 
+	, b.mode as Bin_mode
+	, b.change as Weight_change
+	, b.weight as Total_Weight
+	, b.overweight as Overweight_Status
+	, b.ignore as Ignore_Status 
 FROM sites s
 LEFT OUTER JOIN clusters c 
 	ON s.id = c.site
 LEFT OUTER JOIN  bin_weights b
 	ON c.id = b.unit_id
-WHERE b.timestamp > 'START' 
-	AND b.timestamp < 'END'
-	AND n.unit_id = 'CLUSTER ID'
-ORDER BY b.timestamp; 
+WHERE s.name = 'pint_sf' # 
+ORDER BY b.timestamp
+LIMIT 10; 
+
+# Still debugging why various comands I've been playing with havne't worked by here are some of 
+
+\copy ( SELECT b.unit_id as Cluster_ID, c.name as Cluster_Name, s.name as Organization , b.timestamp as Time_of_Event , b.mode as Bin_mode, b.change as Weight_change, b.weight as Total_Weight, b.overweight as Overweight_Status, b.ignore as Ignore_Status FROM sites s LEFT OUTER JOIN clusters c ON s.id = c.site LEFT OUTER JOIN  bin_weights b ON c.id = b.unit_id WHERE s.name = 'pint_sf'ORDER BY b.timestamp LIMIT 10 ) TO 'test.csv' DELIMITER ',' CSV;
+
+psql -c "COPY ( SELECT b.unit_id as Cluster_ID, c.name as Cluster_Name, s.name as Organization , b.timestamp as Time_of_Event , b.mode as Bin_mode, b.change as Weight_change, b.weight as Total_Weight, b.overweight as Overweight_Status, b.ignore as Ignore_Status FROM sites s LEFT OUTER JOIN clusters c ON s.id = c.site LEFT OUTER JOIN  bin_weights b ON c.id = b.unit_id WHERE s.name = 'pint_sf'ORDER BY b.timestamp LIMIT 10 ) TO STDOUT WITH CSV HEADER " > CSV_FILE.csv
